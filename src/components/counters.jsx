a9aa761c022bbfa2782e37bbfbe293ecb9d17b77
D-coder22 <amitnmf@gmail.com>
2020-08-19 16:31:23 +0530

    Removed local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,18 +3,52 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counter: [
+    counters: [
       { id: 1, value: 4 },
       { id: 2, value: 5 },
       { id: 3, value: 6 },
       { id: 4, value: 7 },
     ],
   };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    counters.value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
-        {this.state.counter.map((counter) => (
-          <Counter key={counter.id} value={counter.value}></Counter>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
+          ></Counter>
         ))}
       </div>
     );
